a5214d25e375ee9b2aa6a1cdf57c3831c8a57259
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2023-10-28 00:10:40 +0800

    Update QueueClass.java

--- Semi-Finals/Activity 1 - Queues/A - Queue Linked List/QueueClass.java
+++ Semi-Finals/Activity 1 - Queues/A - Queue Linked List/QueueClass.java
@@ -32,13 +32,13 @@ public class QueueClass{
          newNode.previous = rear;
          rear = newNode;
       }
-      System.out.println("Enqueued: " + info);
+      System.out.println(info + " Added to queue.");
    }
    
    // DEQUEUE METHOD
    public int dequeue(){
       if (isEmpty()) {
-         System.out.println("Queue is empty.");
+         System.out.println("\nQueue is empty.");
          return -1;
       }
       int dequeuedValue = front.info;
@@ -47,38 +47,38 @@ public class QueueClass{
          rear = null;
       }
       
-      System.out.println("Dequeued: " + dequeuedValue);
+      System.out.println("Element " + dequeuedValue + " Removed from queue.");
       return dequeuedValue;
    }
    
    // FRONT METHOD
    public void front(){
       if (isEmpty()) {
-         System.out.println("Queue is empty.");
+         System.out.println("\nNo front value queue is empty.");
       } 
       else {
-         System.out.println("Front element: " + front.info);
+         System.out.println("\nFront element: " + front.info);
       }
    }
    
    // REAR METHOD
    public void rear(){
       if (isEmpty()) {
-         System.out.println("Queue is empty.");
+         System.out.println("\nNo rear value queue is empty.");
       } 
       else {
-         System.out.println("Rear element: " + rear.info);
+         System.out.println("\nRear element: " + rear.info);
       }
    }
    
    // PRINT METHOD
    public void print(){
       if (isEmpty()) {
-         System.out.println("Queue is empty.");
+         System.out.println("\nQueue is empty.");
       } 
       else {
          QueueNode current = front;
-         System.out.print("Queue: ");
+         System.out.println("\nQueue Contents: ");
          while (current != null) {
             System.out.print(current.info + " ");
             current = current.next;
