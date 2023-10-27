bc2d6078a8a0b1419e26fed57cd894ac6b0aefef
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2023-10-27 23:58:32 +0800

    Update Queue.java

--- Semi-Finals/Activity 1 - Queues/B - Queue Array/Queue.java
+++ Semi-Finals/Activity 1 - Queues/B - Queue Array/Queue.java
@@ -1,40 +1,76 @@
 class Queue {
-    private int[] queueArray;
-    private int front;
-    private int rear;
-    private final int maxSize;
-
-    public Queue(int size) {
-        maxSize = size;
-        queueArray = new int[maxSize];
-        front = 0;
-        rear = -1;
-    }
-
-    public void enqueue(int element) {
-        if (rear < maxSize - 1) {
-            rear++;
-            queueArray[rear] = element;
-            System.out.println(element + " Enqueued in queue");
-        } else {
+   // Variables
+   private int[] queueArray;
+   private int front;
+   private int rear;
+   private final int maxSize;
+   
+   // Constructor
+   public Queue(int size) {
+      maxSize = size;
+      queueArray = new int[maxSize];
+      front = -1;
+      rear = -1;
+   }
+   
+   // Enqueue Method
+   public void enqueue(int element) {
+      if (rear < maxSize - 1) {
+         rear++;
+         queueArray[rear] = element;
+         if (front == -1) {
+            front = 0;
+         }
+            System.out.println(element + " Added in queue");
+      }  else {
             System.out.println("Queue is full. Cannot enqueue more elements.");
-        }
-    }
+         }
+   }
 
-    public void dequeue() {
-        if (front <= rear) {
-            int dequeuedElement = queueArray[front];
+   // Dequeue Method
+   public void dequeue() {
+      if (front <= rear && front != -1) {
+      int dequeuedElement = queueArray[front];
+      if (front == rear) {
+         front = -1;
+         rear = -1;
+      }  else {
             front++;
+         }
             System.out.println("Dequeued element: " + dequeuedElement);
-        } else {
+      }  else {
             System.out.println("Queue is empty. Cannot dequeue elements.");
-        }
-    }
+         }
+   }
+   
+   // Print front Method
+   public void front() {
+      if (front != -1) {
+         System.out.println("Queue Front element: " + queueArray[front]);
+      }  else {
+            System.out.println("No front value Queue is empty.");
+         }
+   }
+   
+   // Print rear Method
+   public void rear() {
+      if (rear != -1) {
+         System.out.println("Queue Rear element: " + queueArray[rear]);
+      }  else {
+            System.out.println("No rear value queue is empty.");
+         }
+   }
 
-    public void display() {
-        System.out.println("Queue Contents:");
-        for (int i = front; i <= rear; i++) {
-            System.out.println(queueArray[i]);
-        }
-    }
+   // Display Method
+   public void display() {
+      if (front != -1) {
+         System.out.println("Queue Contents: ");
+         for (int i = front; i <= rear; i++) {
+            System.out.print(queueArray[i] + " ");
+         } 
+            System.out.println(); // Seperator
+      }  else {
+            System.out.println("Queue is empty.");
+         }
+   }
 }
