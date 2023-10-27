0b78f4b10200bc8d7a03dd50954e753a72b8fc3c
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2023-10-28 00:10:55 +0800

    Update TestQueue.java

--- Semi-Finals/Activity 1 - Queues/A - Queue Linked List/TestQueue.java
+++ Semi-Finals/Activity 1 - Queues/A - Queue Linked List/TestQueue.java
@@ -6,19 +6,19 @@ public class TestQueue{
       Scanner scanner = new Scanner(System.in);
       
       while (true) {
-         System.out.println("\n MENU");
+         System.out.println("\n (MENU)");
          System.out.println("1. Enqueue");
          System.out.println("2. Dequeue");
          System.out.println("3. Print Front");
          System.out.println("4. Print Rear");
          System.out.println("5. Print Queue");
          System.out.println("6. Exit");
-         System.out.print("Enter your choice: ");
+         System.out.print("\nEnter your choice: ");
           
          int choice = scanner.nextInt();
          switch (choice) {
             case 1:
-               System.out.print("Enter element to enqueue: ");
+               System.out.print("\nEnter element to enqueue: ");
                int enqueueValue = scanner.nextInt();
                queue.enqueue(enqueueValue);
                break;
