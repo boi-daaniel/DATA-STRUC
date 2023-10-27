aade68185ed90c16b1f7964d55ed15c6026c2e4a
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2023-10-27 23:58:46 +0800

    Update TestQueue.java

--- Semi-Finals/Activity 1 - Queues/B - Queue Array/TestQueue.java
+++ Semi-Finals/Activity 1 - Queues/B - Queue Array/TestQueue.java
@@ -1,41 +1,49 @@
 import java.util.Scanner;
 
 public class TestQueue {
-    public static void main(String[] args) {
-        final int MAXSIZE = 10;
-        Queue queue = new Queue(MAXSIZE);
-        Scanner scanner = new Scanner(System.in);
+   public static void main(String[] args) {
+      final int MAXSIZE = 5;
+      Queue queue = new Queue(MAXSIZE);
+      Scanner scanner = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("\n \tMENU");
-            System.out.println("1. Enqueue");
-            System.out.println("2. Dequeue");
-            System.out.println("3. Display");
-            System.out.println("0. Exit");
+      while (true) {
+         System.out.println("\n \tMENU");
+         System.out.println("1. Enqueue");
+         System.out.println("2. Dequeue");
+         System.out.println("3. Front");
+         System.out.println("4. Rear");
+         System.out.println("5. Display");
+         System.out.println("0. Exit");
 
-            System.out.print("Enter your choice: ");
-            int choice = scanner.nextInt();
-            System.out.println();
+         System.out.print("Enter your choice: ");
+         int choice = scanner.nextInt();
+         System.out.println();
 
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter element to enqueue: ");
-                    int element = scanner.nextInt();
-                    queue.enqueue(element);
-                    break;
-                case 2:
-                    queue.dequeue();
-                    break;
-                case 3:
-                    queue.display();
-                    break;
-                case 0:
-                    System.out.println("Exiting Program...");
-                    scanner.close();
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid choice. Please enter a valid option.");
-            }
-        }
-    }
+         switch (choice) {
+            case 1:
+               System.out.print("Enter element to enqueue: ");
+               int element = scanner.nextInt();
+               queue.enqueue(element);
+               break;
+            case 2:
+               queue.dequeue();
+               break;
+            case 3:
+               queue.front();
+               break;
+            case 4:
+               queue.rear();
+               break;
+            case 5:
+               queue.display();
+               break;
+            case 0:
+               System.out.println("Exiting Program...");
+               scanner.close();
+               System.exit(0);
+            default:
+               System.out.println("Invalid choice. Please enter a valid option.");
+         }
+      }
+   }
 }
