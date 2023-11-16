03f61d68c31e66e16c4383f6dbfbc6bfdcf7a1ea
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2023-11-16 12:34:10 +0800

    Update TestBSTClass.java

--- Semi-Finals/Binary/TestBSTClass.java
+++ Semi-Finals/Binary/TestBSTClass.java
@@ -12,7 +12,10 @@ public class TestBSTClass {
             System.out.println("2. Inorder Traversal");
             System.out.println("3. Preorder Traversal");
             System.out.println("4. Postorder Traversal");
-            System.out.println("5. Exit");
+            System.out.println("5. Get Tree Height");
+            System.out.println("6. Get Node Count");
+            System.out.println("7. Search");
+            System.out.println("8. Exit");
             System.out.print("Enter your choice: ");
             choice = scanner.nextInt();
 
@@ -35,14 +38,29 @@ public class TestBSTClass {
                     bst.postorder(bst.root);
                     break;
                 case 5:
+                    System.out.println("Tree Height: " + bst.getHeight());
+                    break;
+                case 6:
+                    System.out.println("Node Count: " + bst.getNodeCount());
+                    break;
+                case 7:
+                    System.out.print("Enter value to search: ");
+                    int searchValue = scanner.nextInt();
+                    if (bst.search(searchValue)) {
+                        System.out.println(searchValue + " found in the tree.");
+                    } else {
+                        System.out.println(searchValue + " not found in the tree.");
+                    }
+                    break;
+                case 8:
                     System.out.println("Exiting... Bye!");
                     break;
                 default:
                     System.out.println("Invalid choice. Please enter a valid option.");
                     break;
             }
-        } while (choice != 5);
+        } while (choice != 8);
 
         scanner.close();
     }
-}
+        }
