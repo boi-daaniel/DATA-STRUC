5d72f5f002b1af9fce969dc409ab6c287480033a
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2023-11-16 13:17:15 +0800

    Update BSTClass.java

--- Semi-Finals/Binary/BSTClass.java
+++ Semi-Finals/Binary/BSTClass.java
@@ -13,41 +13,79 @@ public class BSTClass extends BTClass {
     public boolean isEmpty() {
         return super.isEmpty();
     }
+    
+    public boolean search(int value) {
+        return searchValue(root, value);
+    }
 
-    public void insertNode(int insertItem){
+    private boolean searchValue(BTNode root, int value) {
+        if (root == null) {
+            return false;
+        } else if (root.info == value) {
+            return true;
+        } else if (value < root.info) {
+            return searchValue(root.Llink, value);
+        } else {
+            return searchValue(root.Rlink, value);
+        }
+    }
+
+    public int getNodeCount() {
+        return countNodes(root);
+    }
+
+    private int countNodes(BTNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + countNodes(root.Llink) + countNodes(root.Rlink);
+    }
+
+    public int height() {
+        return calculateHeight(root);
+    }
+
+    private int calculateHeight(BTNode root) {
+        if (root == null) {
+            return -1; // An empty tree has a height of -1
+        }
+        int leftHeight = calculateHeight(root.Llink);
+        int rightHeight = calculateHeight(root.Rlink);
+        return 1 + Math.max(leftHeight, rightHeight);
+    }
+
+    public void insertNode(int insertItem) {
         BTNode newNode = new BTNode();
-        newNode.Llink = null;
         newNode.info = insertItem;
+        newNode.Llink = null;
         newNode.Rlink = null;
 
         BTNode current;
-        BTNode trailcurrent = null;
+        BTNode trailCurrent = null;
+
         if (root == null) {
             root = newNode;
-        } 
-        else {
+        } else {
             current = root;
             while (current != null) {
-                trailcurrent = current;
-                if (current.info == insertItem) {
+                trailCurrent = current;
+                if (insertItem == current.info) {
                     System.out.println("Item already exists.");
-                } 
-                else {
-                    if (insertItem < current.info) {
-                        current = current.Llink;
-                    } 
-                    else {
-                        current = current.Rlink;
-                    }
+                    return;
+                } else if (insertItem < current.info) {
+                    current = current.Llink;
+                } else {
+                    current = current.Rlink;
                 }
             }
-            if (insertItem < trailcurrent.info) {
-                trailcurrent.Llink = newNode;
-            } 
-            else {
-                trailcurrent.Rlink = newNode;  
+
+            if (insertItem < trailCurrent.info) {
+                trailCurrent.Llink = newNode;
+            } else {
+                trailCurrent.Rlink = newNode;
             }
         }
     }
-  
 }
+  
+
