7634860e52b9c019592d92edfd2f61e6f15cbf6c
boi daniel <142441884+boi-daaniel@users.noreply.github.com>
2023-11-16 12:59:34 +0800

    Update TestBSTClass.java

--- Semi-Finals/Binary/TestBSTClass.java
+++ Semi-Finals/Binary/TestBSTClass.java
@@ -3,21 +3,21 @@ import java.util.Scanner;
 public class TestBSTClass {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        BTClass bst = new BTClass();
+        BSTClass bst = new BSTClass();
 
-        int choice;
-        do {
+        while(true) {
             System.out.println("\nBinary Search Tree Operations:");
             System.out.println("1. Insert Node");
-            System.out.println("2. Inorder Traversal");
-            System.out.println("3. Preorder Traversal");
-            System.out.println("4. Postorder Traversal");
-            System.out.println("5. Get Tree Height");
-            System.out.println("6. Get Node Count");
-            System.out.println("7. Search");
-            System.out.println("8. Exit");
-            System.out.print("Enter your choice: ");
-            choice = scanner.nextInt();
+            System.out.println("2. Search");
+            System.out.println("3. Node Count");
+            System.out.println("4. Height");
+            System.out.println("5. Inorder Traversal");
+            System.out.println("6. Preorder Traversal");
+            System.out.println("7. Postorder Traversal");
+            System.out.println("0. Exit");
+            
+            System.out.print("\nEnter your choice: ");
+            int choice = scanner.nextInt();
 
             switch (choice) {
                 case 1:
@@ -26,24 +26,6 @@ public class TestBSTClass {
                     bst.insertNode(value);
                     break;
                 case 2:
-                    System.out.println("Inorder Traversal:");
-                    bst.inorder(bst.root);
-                    break;
-                case 3:
-                    System.out.println("Preorder Traversal:");
-                    bst.preorder(bst.root);
-                    break;
-                case 4:
-                    System.out.println("Postorder Traversal:");
-                    bst.postorder(bst.root);
-                    break;
-                case 5:
-                    System.out.println("Tree Height: " + bst.height());
-                    break;
-                case 6:
-                    System.out.println("Node Count: " + bst.getNodeCount());
-                    break;
-                case 7:
                     System.out.print("Enter value to search: ");
                     int searchValue = scanner.nextInt();
                     if (bst.search(searchValue)) {
@@ -51,16 +33,32 @@ public class TestBSTClass {
                     } else {
                         System.out.println(searchValue + " not found in the tree.");
                     }
+                    break;                  
+                case 3:
+                    System.out.println("Node Count: " + bst.getNodeCount());
+                    break;                 
+                case 4:
+                    System.out.println("Tree Height: " + bst.height());
+                    break;                   
+                case 5:
+                    System.out.println("Inorder Traversal:");
+                    bst.inorder(bst.root);
+                    break;                
+                case 6:
+                    System.out.println("Preorder Traversal:");
+                    bst.preorder(bst.root);
                     break;
-                case 8:
-                    System.out.println("Exiting... Bye!");
+                case 7:
+                    System.out.println("Postorder Traversal:");
+                    bst.postorder(bst.root);
+                    break;                    
+                case 0:
+                    System.out.println("Exiting...");
                     break;
                 default:
                     System.out.println("Invalid choice. Please enter a valid option.");
                     break;
             }
-        } while (choice != 8);
-
-        scanner.close();
+        } 
     }
 }
